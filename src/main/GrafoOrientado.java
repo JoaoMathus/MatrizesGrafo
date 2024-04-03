e82b387e88090820e1b5d7647e95fb970236e368
João Oliveira <joaomatheusGDO@gmail.com>
2024-04-03 20:54:54 -0300

    Fim do trabalho: conversão de matrizes de incidência para matrizes de adjacências com grafos orientados e não orientados.

--- src/main/GrafoOrientado.java
+++ src/main/GrafoOrientado.java
@@ -1,20 +1,54 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GrafoOrientado {
     public static int[][] criarIncidencia(int numeroVertices, int[][] arestas) {
         int numeroArestas = arestas.length;
         int[][] incidencia = new int[numeroVertices][numeroArestas];
 
-
+        for (int i = 0; i < numeroArestas; i++) {
+            int v1 = arestas[i][0];
+            int v2 = arestas[i][1];
+            if (v1 == v2) {
+                incidencia[v1-1][i] += 1;
+            } else {
+                incidencia[v1-1][i] += 1;
+                incidencia[v2-1][i] -= 1;
+            }
+        }
 
         return incidencia;
     }
 
     public static int[][] incidenciaParaAdjacencias(int[][] incidencia) {
         int numeroVertices = incidencia.length;
-        // int numeroArestas = incidencia[0].length;
+        int numeroArestas = incidencia[0].length;
         int[][] adjacencias = new int[numeroVertices][numeroVertices];
+        List<Integer> verticesConectados = new ArrayList<>();
 
+        for (int i = 0; i < numeroArestas; i++) {
+            for (int j = 0; j < numeroVertices; j++) {
+                if (incidencia[j][i] != 0) {
+                    verticesConectados.add(j);
+                }
+            }
+            if (verticesConectados.size() == 2) {
+                int v1 = verticesConectados.get(0);
+                int v2 = verticesConectados.get(1);
+                if (incidencia[v1][i] > 0) {
+                    adjacencias[v1][v2] += 1;
+                } else {
+                    adjacencias[v2][v1] += 1;
+                }
+                verticesConectados.clear();
+            } else if (verticesConectados.size() == 1) {
+                int v1 = verticesConectados.get(0);
+                adjacencias[v1][v1] += 1;
+                verticesConectados.clear();
+            }
+        }
 
         return adjacencias;
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,10 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
+        // Não orientado
         int numeroVertices = 5;
         int[][] arestas = {
+                {1,1},
                 {1,2},
                 {1,2},
                 {1,3},
@@ -11,6 +13,18 @@ public class Main {
                 {3,2},
                 {3,4},
                 {4,5}
+        };
+
+        // Orientado
+        int verticesOrientados = 5;
+        int[][] setas = {
+                {1,3},
+                {1,4},
+                {2,1},
+                {2,2},
+                {3,5},
+                {5,2},
+                {5,3}
         };
 
         System.out.println(":: Grafos não orientados ::\n");
@@ -32,5 +46,15 @@ public class Main {
         System.out.println();
         System.out.println();
 
+        System.out.println(":: Grafo orientado ::");
+
+        System.out.println("Matriz de incidência:");
+        java.util.Arrays.stream(GrafoOrientado.criarIncidencia(verticesOrientados, setas))
+                .forEach(linha -> System.out.println(java.util.Arrays.toString(linha)));
+
+        System.out.println("Matriz de adjacências:");
+        java.util.Arrays.stream(GrafoOrientado.incidenciaParaAdjacencias(
+                GrafoOrientado.criarIncidencia(verticesOrientados, setas)
+        )).forEach(linha -> System.out.println(java.util.Arrays.toString(linha)));
     }
 }
